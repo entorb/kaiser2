20c05aaec2fec014b375a64cdfb97c4448bd7ec1
Torben <59419684+entorb@users.noreply.github.com>
2026-09-20 22:53:19 +0200

    Scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-
 cd "$(dirname "$0")/.." || exit 1
 
 # exit upon error
 set -e
 
+# cleanup
+rm -f .DS_Store
+rm -f -- ./*/.DS_Store
+
+echo "## Checks"
+echo "### Code checks"
 ./scripts/run_checks.sh
 
+echo "## Frontend Build and Transfer"
 pnpm run build
-rsync -rhv --delete --no-perms dist/* entorb@entorb.net:html/kaiser2/
+rsync -rhv --delete --no-perms dist/ entorb@entorb.net:html/kaiser2/
+
+echo "## DONE"
